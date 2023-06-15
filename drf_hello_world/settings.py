a367b6dc230942a4cfdf7bda2b6bf4f7f07d13e1
Aniket Aryamane <aniket.aryamane@fafadiatech.com>
2023-06-15 18:26:07 +0530

    DRF hello world

--- drf_hello_world/settings.py
+++ drf_hello_world/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n21upflo#48_!82lvsp8)-jh)pq@m337@yibih3_!v+@vbd)*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.51"]
 
 
 # Application definition
@@ -123,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "static"
