739ba99bf8b78b9e54828caad80771bb8050bb3e
Aniket Aryamane <aniket.aryamane@fafadiatech.com>
2023-06-16 18:03:28 +0530

    Final Code

--- allapps/core/views.py
+++ allapps/core/views.py
@@ -6,4 +6,4 @@ from rest_framework.views import APIView
 # Create your views here.
 class HomeView(APIView):
     def get(self, request):
-        return Response({"message": "Hello World!"}, status=status.HTTP_200_OK)
+        return Response({"message": "Hello from Fafadia Tech!"}, status=status.HTTP_200_OK)
--- drf_hello_world/urls.py
+++ drf_hello_world/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("allapps.core.urls", namespace="core")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
